c382bb83cf088dc72efec53acb11ccf56d13fc28
szatkus <szatkus@gmail.com>
2011-04-05 19:56:57 +0200

    Monsters

--- e1/rpgdb.py
+++ e1/rpgdb.py
@@ -21,9 +21,22 @@ def create(name):
 		strength.atype = "ability"
 		return(strength)
 	
+	#Life
+	if name == "life":
+		life = extsea.Attribute("life")
+		life.atype = "ability"
+		return(life)
+	
+	#Speed
+	if name == "speed":
+		speed = extsea.Attribute("speed")
+		speed.atype = "ability"
+		return(speed)
+	
 	#Sword
 	if name == "sword":
 		sword = extsea.Attribute("sword")
+		sword.multi *= 4
 		sword.dep = ["strength"]
 		sword.atype = "skill"
 		return(sword)
@@ -31,13 +44,50 @@ def create(name):
 	#Human
 	if name == "human":
 		human = extsea.Attribute("human")
-		human.affect = ["strength", "speed"]
+		human.affect = ["strength", "speed", "life"]
 		human.max_level = 1
 		def human_mod(self, mod):
-			mod.tbonus *= 5
+			if mod.name == "life":
+				mod.tbonus *= 10
+			if mod.name == "strength":
+				mod.tbonus *= 4
+			if mod.name == "speed":
+				mod.tbonus *= 5
 		human.mod = human_mod
 		human.atype = "race"
 		return(human)
+	
+	#Wolf
+	if name == "wolf":
+		wolf = extsea.Attribute("wolf")
+		wolf.affect = ["strength", "speed", "life"]
+		wolf.max_level = 1
+		def wolf_mod(self, mod):
+			if mod.name == "life":
+				mod.tbonus *= 8
+			if mod.name == "strength":
+				mod.tbonus *= 5
+			if mod.name == "speed":
+				mod.tbonus *= 7
+		wolf.mod = wolf_mod
+		wolf.atype = "race"
+		return(wolf)
+	
+	#Mushroom
+	if name == "mushroom":
+		mushroom = extsea.Attribute("mushroom")
+		mushroom.affect = ["strength", "speed", "life"]
+		mushroom.max_level = 1
+		def mushroom_mod(self, mod):
+			if mod.name == "life":
+				mod.tbonus *= 5
+			if mod.name == "strength":
+				mod.tbonus *= 2
+			if mod.name == "speed":
+				mod.tbonus *= 2
+		mushroom.mod = mushroom_mod
+		mushroom.atype = "race"
+		return(mushroom)
 		
 	#Male
 	if name == "male":
@@ -50,6 +100,27 @@ def create(name):
 		male.atype = "gender"
 		return(male)
 	
+	#Female
+	if name == "female":
+		female = extsea.Attribute("female")
+		female.affect = ["speed", "magic"]
+		female.max_level = 1
+		def female_mod(self, mod):
+			mod.tbonus *= 1.2
+		female.mod = female_mod
+		female.atype = "gender"
+		return(female)
+	
+	#Bite
+	if name == "bite":
+		bite = extsea.Attribute("bite")
+		bite.deps = ["strength"]
+		def bite_use(self, mod):
+			mod.tbonus *= 1.2
+		bite.use = bite_use
+		bite.atype = "attack"
+		return(bite)
+	
 	return(None)
 
 def createl(name, level):
@@ -59,3 +130,19 @@ def createl(name, level):
 	return(attrib)
 
 
+def create_monster(name):
+	if name == "wolf":
+		wolf = extsea.Character("Wolf")
+		wolf.add(create("wolf"))
+		wolf.add(createl("strength", 4))
+		wolf.add(createl("speed", 4))
+		wolf.add(createl("life", 4))
+		return(wolf)
+	
+	if name == "mushroom":
+		mushroom = extsea.Character("Mushroom")
+		mushroom.add(create("mushroom"))
+		mushroom.add(createl("strength", 4))
+		mushroom.add(createl("speed", 4))
+		mushroom.add(createl("life", 4))
+		return(mushroom)
--- e1/test.py
+++ e1/test.py
@@ -8,6 +8,12 @@ c.add(rpgdb.createl("sword", 4))
 c.add(rpgdb.create("male"))
 c.add(rpgdb.createl("strength", 3))
 c.add(rpgdb.create("human"))
-print(c.find_attrib("dupa")[0].name)
 
-rpgdb.viewc(c)
+#rpgdb.viewc(c)
+
+w = rpgdb.create_monster("wolf")
+m = rpgdb.create_monster("mushroom")
+
+rpgdb.viewc(w)
+print(w.attrib["speed"].dep[0].name)
+rpgdb.viewc(m)
